dfeb065f0c1fc51d276bf9577c47b787f28c95fd
Redszen <jmireles005@student.butte.edu>
2026-09-07 03:57:23 +0000

    lab final submit

---
+++ 9/3/lab.cpp
@@ -0,0 +1,28 @@
+//library include
+// # include<library name>
+#include<iostream>
+// # is there because it is apart of the combination of letters "include" in the context of c++ to effectively activate its intended purpose which is to give the code a data base
+//specific objects from library
+using std::cout;
+//line 6 "using" tells the code what part of the library we want it to take from
+//std::cout is the output of the function you coded essentially. 
+//; is meant to end a statement. 
+//std is where everything in the c++ library lives and it includes all standard purposes in c++
+//the two : are there to tell the code the scope of what you typed. 
+// entrypoint (main refers to the entrypoint of c++)
+// the entry point is where the code begins/runs
+//int is a combonation of letters which represent integers. this tells the code you want to have a return value of an integer. 
+int main() {
+cout << "hello world";
+    return 0; 
+}
+//main is the portion of the code that the program looks for initially to execute the instructions inside.
+//the parentheses surrounding an empty space next to main shows us this is a function of sorts.
+//The open bracket ({) shows the program where the instructions which belong to main begin. The closed bracket shows where it ends.
+//cout means character output, and it tells the program to send something to the screen to display characters.
+//the << symbols are called "Stream insertion operator", and it tells the program what characters to output.
+//hello world is a string. That is, a combination of letters to make two words which will be displayed.
+//The quote marks tells the program what should be treated as text.
+//using ; at the end tells the program the instructions are finished. 
+//return is what we need to put in order to have a returning integer from the earlier portion labeled int main()
+//the 0 beside the return says we need to have a return integer value of 0.
